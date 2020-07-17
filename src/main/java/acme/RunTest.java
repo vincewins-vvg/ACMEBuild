f83abf57f98d86efa8507caaa93b49c149b51898
Gopinath Jayakumar <gopirandom@gmail.com>
2020-07-18 01:22:34 +0530

    updated Snapshot

--- src/main/java/acme/RunTest.java
+++ src/main/java/acme/RunTest.java
@@ -1,9 +1,14 @@
 package acme;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -11,11 +16,13 @@ import org.testng.annotations.Test;
 
 public class RunTest {
 	
+	public ChromeDriver driver;
+	
 	@Test
 	public void runTest() throws InterruptedException { // for chrome
 		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://acme-test.uipath.com/");
@@ -32,11 +39,21 @@ public class RunTest {
 		driver.findElement(By.id("vendorName")).sendKeys("Blue Lagoon");
 		driver.findElement(By.id("buttonSearch")).click();
 		String country = driver.findElement(By.xpath("(//td[text()='Blue Lagoon']/following-sibling::td)[4]")).getText();
+		takeSnap();
 		System.out.println(country);
 		driver.findElement(By.linkText("Log Out")).click();
-		driver.close();
-		
-		
+		driver.close();		
 	}
 
+	public void takeSnap() {
+		try {
+			FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE), new File("./snaps/1.png"));
+		} catch (WebDriverException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
